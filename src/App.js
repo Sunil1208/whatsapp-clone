2888b0a872a155b208183c2060f5446e62ef9c13
Sunil <sunil.k@goodbox.in>
2020-12-15 11:04:43 +0530

    sidepar WIP

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.css';
+import Sidebar from './components/sidebar/Sidebar';
+
 
 function App() {
   return (
     // BEM naming convention
-    <div className="App">
-      <h1>Let's build a Whatsapp Clone!</h1>
-      <div className="app_body">
-      {/* Sidebar */}
+    <div className="app">
+      <div className="app__body">
+      <Sidebar />
       {/* Chat */}
       </div>
     </div>
